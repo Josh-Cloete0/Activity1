1d0785f8077abe402b63d186c4f1cb13aa317d00
Josh-Cloete0 <st10473468@vcconnect.edu.za>
2025-08-08 15:05:25 +0200

    Second commit added new variables and styles

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image,Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Image,Button, StyleSheet, Text, TextInput, View,} from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const[Name,setName] = useState('');
+  const[Surname,setSurname] = useState('');
+  console.log("App Syarting up now.")
+
   return (
-    <View style={styles.container}>
+    <View style={styles.welcomeText}>
       <Text style={styles.welcomeText}>Welcome to your React App!</Text>
-      <Text>Enter Name:</Text>
-      <TextInput placeholder='First Name'/>
-      <Text>Enter Surname:</Text>
-      <TextInput placeholder='Surname'/>
-      <Button title='Add User'/>
-      
+      <View style={styles.InputFlex}>
+      <Text style={styles.HeadingText}>Enter Name:</Text>
+      <TextInput style={styles.InputBoxs} placeholder='First Name' onChangeText={newText=>setName(newText)}/>
+
+
+
+      </View>
+      <Text style={styles.HeadingText}>Enter Surname:</Text>
+      <TextInput style={styles.InputBoxs} placeholder='Surname' onChangeText={newText=>setSurname(newText)}/>
+      <Button title='Add User' onPress={()=>{console.log("The users name is: " + Name +" Surname:"+ Surname)}}/>
       <StatusBar style="auto" />
-      <View>
+    <View style={styles.mainPicture}>
         <Image source={require('./Images/Welcome_to_react.png')}/>
       </View>
+      
+      
     </View>
   );
 }
@@ -26,5 +37,27 @@ const styles = StyleSheet.create({
     fontWeight:'bold',
     fontSize:20,
     textAlign:'center'
-  }
+  },
+
+  ImageSize:{
+    width:350,
+    height:350
+
+  },
+
+  mainPicture:{
+    paddingTop:40,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+
+  InputFlex:{
+    flexDirection:'row',
+    marginTop:30,
+    justifyContent:'space-evenly'
+  },
+
+  HeadingText:{},
+
+  InputBoxs:{},
 });
