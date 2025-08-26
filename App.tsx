b12774130fc0998d66b1f7d968d6252184bd07f5
Josh-Cloete0 <st10473468@vcconnect.edu.za>
2025-08-26 15:19:54 +0200

    Added Second screen and transferred data

--- App.tsx
+++ App.tsx
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image,Button, StyleSheet, Text, TextInput, View,} from 'react-native';
 import { useState } from 'react';
+import{createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+const Stack= createNativeStackNavigator();
 
 export default function App() {
+  return(
+  <NavigationContainer> 
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={MainScreen} />
+      <Stack.Screen name="ViewDetails" component={ViewDetails}/>
+    </Stack.Navigator>
+  </NavigationContainer>
+
+);
+};
+function MainScreen({navigation}) {
+  
   const[Name,setName] = useState('');
   const[Surname,setSurname] = useState('');
   console.log("App Syarting up now.")
-
+ 
   return (
     <View style={styles.welcomeText}>
       <Text style={styles.welcomeText}>Welcome to your React App!</Text>
@@ -19,7 +34,15 @@ export default function App() {
       </View>
       <Text style={styles.HeadingText}>Enter Surname:</Text>
       <TextInput style={styles.InputBoxs} placeholder='Surname' onChangeText={newText=>setSurname(newText)}/>
-      <Button title='Add User' onPress={()=>{console.log("The users name is: " + Name +" Surname:"+ Surname)}}/>
+      
+      <Button title = "Add User"
+    onPress={() =>{
+      navigation.navigate('ViewDetails',{
+        NameSend : Name,
+        SurnameSend : Surname});
+        console.log("Name:" +Name+"Surname:" + Surname)}}
+        />
+
       <StatusBar style="auto" />
     <View style={styles.mainPicture}>
         <Image source={require('./Images/Welcome_to_react.png')}/>
@@ -27,9 +50,21 @@ export default function App() {
       
       
     </View>
+
   );
 }
+ function ViewDetails({navigation, route})
+ {
+  const NameGet = route.params.NameSend;
+  const SurnameGet= route.params.SurnameSend;
 
+  return(
+    <View style={{flex: 1, alignItems: 'center',justifyContent: 'center'}}>
+      <Text>Name : {NameGet} Surname:{SurnameGet}</Text>
+    </View>
+
+
+  )}
 const styles = StyleSheet.create({
   welcomeText:{
     paddingTop:40,
